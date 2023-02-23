7c75ee08c99af18d7df5e151e736b7d016565b9d
Laryssa Paiva <laryssapaiva@id.uff.br>
2023-02-23 13:46:51 -0300

    frequencia rj

--- frequencia.py
+++ frequencia.py
@@ -6,19 +6,20 @@ import numpy as np
 # FILTRO DE PONTOS DE ÔNIBUS QUE POSSUEM UMA FREQUÊNCIA 
 # MAIOR QUE 5 ÔNIBUS POR HORA EM HORÁRIOS DE PICO (7H-10H) E (17H-20H) 
 
-
 # convert string to datatime (df: stop_times)
-stop_times = pd.read_csv('dados\paradas\stop_times.txt', sep=',')
+stop_times = pd.read_csv('dados\carioca\stop_times.txt', sep=',')
 stop_times['arrival_time'] = pd.to_datetime(stop_times['arrival_time'])
 stop_times['hour'] = stop_times['arrival_time'].dt.hour
+print(stop_times.head())
 
 # df stops
-stops = pd.read_csv('dados\paradas\stops.txt', sep=',')
+stops = pd.read_csv('dados\carioca\stops.txt', sep=',')
+print(stops.head())
 
 # merge stop_times & stops
 events = pd.merge(stop_times, stops, on='stop_id')
-""" 
-print(events.groupby('stop_id').sum().describe()) """
+print(events.head())
+
 
 # filter events by time: 7h-10h & 17h-20h
 filter_es_morning = events.loc[(events['hour'] >= 7) & (events['hour'] < 9)].groupby('stop_id', as_index=False).count()
@@ -29,7 +30,7 @@ merge = pd.merge(stops,filter_es_morning, on='stop_id')
 print(merge.describe())
 
 merge_qtd = merge.loc[(merge['qtd'] >= 10)]
-print(merge.describe())
+print(merge_qtd.describe())
 """ 
 fig = px.scatter(merge, x='stop_lon', y='stop_lat',
                  color='qtd')
@@ -56,9 +57,7 @@ fig.update_layout(
     margin=dict(l=20, r=20, t=20, b=20),
     paper_bgcolor="LightSteelBlue",
 )
-fig.show()
-
-fig.show()
+#fig.show()
 
 
 
